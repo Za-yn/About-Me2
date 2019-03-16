65cc92bd2138e36fb397eed5430e686a0aea904b
za-yn <yuan.z@u.nus.edu>
2019-03-17 01:29:31 +0800

    bug fixes 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Resume from 'react-awesome-resume'
 import logo from './logo.svg';
 import './App.css';
 
-const myJSONResume = {data: "../public/resume.json"};
+const myJSONResume = {basics: "../public/resume.json"};
 
 class App extends Component {
   render() {
