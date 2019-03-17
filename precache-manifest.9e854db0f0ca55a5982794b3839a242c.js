d03dd216d8bedd1e86f88743e6d717f0accd0761
za-yn <yuan.z@u.nus.edu>
2019-03-17 11:27:51 +0800

    Updates

--- precache-manifest.9e854db0f0ca55a5982794b3839a242c.js
+++ precache-manifest.9e854db0f0ca55a5982794b3839a242c.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/About-Me2/static/media/brand-icons.e8c322de.woff2"
   },
   {
-    "revision": "42c4947ef3354e4f75fe",
+    "revision": "c00eeb634cdc8a4e08e7",
     "url": "/About-Me2/static/css/main.07318036.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = [
     "url": "/About-Me2/static/media/logo.5d5d9eef.svg"
   },
   {
-    "revision": "b10f5d799e981d0911ee",
-    "url": "/About-Me2/static/js/2.b453e051.chunk.js"
-  },
-  {
-    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
-    "url": "/About-Me2/static/media/outline-icons.ef60a4f6.woff"
+    "revision": "e8573435432a70acd991",
+    "url": "/About-Me2/static/js/2.66e133c6.chunk.js"
   },
   {
     "revision": "cd6c777f1945164224dee082abaea03a",
     "url": "/About-Me2/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/About-Me2/static/media/outline-icons.ef60a4f6.woff"
   },
   {
     "revision": "701ae6abd4719e9c2ada3535a497b341",
@@ -44,36 +44,36 @@ self.__precacheManifest = [
     "url": "/About-Me2/static/media/icons.faff9214.woff"
   },
   {
-    "revision": "42c4947ef3354e4f75fe",
-    "url": "/About-Me2/static/js/main.0a16abe9.chunk.js"
+    "revision": "c00eeb634cdc8a4e08e7",
+    "url": "/About-Me2/static/js/main.df60a961.chunk.js"
   },
   {
     "revision": "a046592bac8f2fd96e994733faf3858c",
     "url": "/About-Me2/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/About-Me2/static/media/icons.b87b9ba5.ttf"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/About-Me2/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/About-Me2/static/media/brand-icons.c5ebe0b3.ttf"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/About-Me2/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/About-Me2/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "9c74e172f87984c48ddf5c8108cabe67",
-    "url": "/About-Me2/static/media/flags.9c74e172.png"
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/About-Me2/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
     "url": "/About-Me2/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/About-Me2/static/media/flags.9c74e172.png"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
@@ -84,11 +84,11 @@ self.__precacheManifest = [
     "url": "/About-Me2/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "b10f5d799e981d0911ee",
+    "revision": "e8573435432a70acd991",
     "url": "/About-Me2/static/css/2.c3a5a412.chunk.css"
   },
   {
-    "revision": "d7615828af922f909d3485cd89699ef2",
+    "revision": "ba5a80c16fbafd1a3533e7ef5a698927",
     "url": "/About-Me2/index.html"
   }
 ];
